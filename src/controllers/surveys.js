7fb9159d77164ab56e0112daf46119c5ba2193bd
Firmansyah Rozy <rozy.firmansyah@gmail.com>
2019-11-29 03:24:18 +0700

    create survey controllers

--- src/controllers/surveys.js
+++ src/controllers/surveys.js
@@ -1,4 +1,10 @@
-const { getAllSurveys, getSingleSurvey } = require("../models/surveys");
+const {
+  getAllSurveys,
+  getSingleSurvey,
+  addSurvey,
+  editSurvey,
+  deleteSurvey
+} = require("../models/surveys");
 
 module.exports = {
   getAllSurveys: (req, res) => {
@@ -7,7 +13,27 @@ module.exports = {
       .catch(error => res.json(error));
   },
   getSingleSurvey: (req, res) => {
-    getSingleSurvey(req.params.id)
+    const id = req.params.id;
+    getSingleSurvey(id)
+      .then(response => res.json(response[0]))
+      .catch(error => res.json(error));
+  },
+  addSurvey: (req, res) => {
+    const data = { ...req.body };
+    addSurvey(data)
+      .then(response => res.json(response))
+      .catch(error => res.json(error));
+  },
+  editSurvey: (req, res) => {
+    const id = req.params.id;
+    const data = { ...req.body };
+    editSurvey(id, data)
+      .then(response => res.json(response))
+      .catch(error => res.json(error));
+  },
+  deleteSurvey: (req, res) => {
+    const id = req.params.id;
+    deleteSurvey(id)
       .then(response => res.json(response[0]))
       .catch(error => res.json(error));
   }
